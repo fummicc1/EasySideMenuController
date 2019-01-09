dd2c28f28df64376619dcb2b62d9c4a6590ad31d
fummicc1-sub <fumiya989@gmail.com>
2019-01-09 20:48:06 +0900

    first commit

---
+++ EasySideMenuController/EasySideMenuController.h
@@ -0,0 +1,19 @@
+//
+//  EasySideMenuController.h
+//  EasySideMenuController
+//
+//  Created by Fumiya Tanaka on 2019/01/09.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasySideMenuController.
+FOUNDATION_EXPORT double EasySideMenuControllerVersionNumber;
+
+//! Project version string for EasySideMenuController.
+FOUNDATION_EXPORT const unsigned char EasySideMenuControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasySideMenuController/PublicHeader.h>
+
+
