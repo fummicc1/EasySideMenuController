92ca7cc3ed0818b21a368748439a1b908d6c69de
fummicc1 <fumiya989@gmail.com>
2019-01-21 09:44:09 +0900

    sample

---
+++ EasySideMenuController/EasyPresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  PresentationController.swift
+//  EasySideMenuController
+//
+//  Created by Fumiya Tanaka on 2019/01/21.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import Foundation
+
+public class EasyPresentationController: UIPresentationController {
+    
+}
---
+++ EasySideMenuController/SideMenuModel.swift
@@ -0,0 +1,9 @@
+//
+//  SideMenuModel.swift
+//  EasySideMenuController
+//
+//  Created by Fumiya Tanaka on 2019/01/21.
+//  Copyright © 2019 Fumiya Tanaka. All rights reserved.
+//
+
+import Foundation
