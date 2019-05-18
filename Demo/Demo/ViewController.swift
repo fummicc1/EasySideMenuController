e30907bda73141f7c17e5483d245e52233a0554c
fummicc1 <fumiya.tennis1@gmail.com>
2019-05-18 11:50:15 +0900

    nothing change

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -17,21 +17,41 @@ class ViewController: EasySideMenuController {
 
     @IBAction func didTapLeftSideMenuButton() {
         let modalVC = UIStoryboard(name: "Left", bundle: nil).instantiateInitialViewController() as! LeftModalViewController
-        SideMenuManager.shared.showSideMenu(direction: .left, presenting: self, presented: modalVC, sideMenuSize: CGSize(width: 150, height: UIScreen.main.bounds.height))
+        SideMenuManager.shared.showSideMenu(
+            direction: .left,
+            presenting: self,
+            presented: modalVC,
+            sideMenuSize: CGSize(width: 150, height: UIScreen.main.bounds.height)
+        )
     }
     
     @IBAction func didTapRightSideMenuButton() {
         let modalVC = UIStoryboard(name: "Right", bundle: nil).instantiateInitialViewController() as! RightModalViewController
-        SideMenuManager.shared.showSideMenu(direction: .right, presenting: self, presented: modalVC, sideMenuSize: CGSize(width: 150, height: UIScreen.main.bounds.height))        
+        SideMenuManager.shared.showSideMenu(
+            direction: .right,
+            presenting: self,
+            presented: modalVC,
+            sideMenuSize: CGSize(width: 150, height: UIScreen.main.bounds.height)
+        )
     }
     
     @IBAction func didTapTopSideMenuButton() {
         let modalVC = UIStoryboard(name: "Top", bundle: nil).instantiateInitialViewController() as! TopModalViewController
-        SideMenuManager.shared.showSideMenu(direction: .top, presenting: self, presented: modalVC, sideMenuSize: CGSize(width: UIScreen.main.bounds.width, height: 200))
+        SideMenuManager.shared.showSideMenu(
+            direction: .top,
+            presenting: self,
+            presented: modalVC,
+            sideMenuSize: CGSize(width: UIScreen.main.bounds.width, height: 200)
+        )
     }
     
     @IBAction func didTapBottomSideMenuButton() {
         let modalVC = UIStoryboard(name: "Top", bundle: nil).instantiateInitialViewController() as! TopModalViewController
-        SideMenuManager.shared.showSideMenu(direction: .bottom, presenting: self, presented: modalVC, sideMenuSize: CGSize(width: UIScreen.main.bounds.width, height: 200))
+        SideMenuManager.shared.showSideMenu(
+            direction: .bottom,
+            presenting: self,
+            presented: modalVC,
+            sideMenuSize: CGSize(width: UIScreen.main.bounds.width, height: modalVC.view.bounds.height)
+        )
     }
 }
